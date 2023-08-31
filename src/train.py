cfbcaafe7862e2a0300778b317d9b261a809d063
Daniyal-Mackey <daniyal.mackeyabadi@divar.ir>
2023-08-31 12:36:13 +0330

    DBSCAN

--- src/train.py
+++ src/train.py
@@ -1,11 +1,17 @@
 import mlflow
 import numpy as np
 import pandas as pd
+from sklearn.cluster import DBSCAN
 
 
 def train():
     # your code
-    df = pd.read_csv("")
+    df = pd.read_csv("https://kwargs.s3.ir-thr-at1.arvanstorage.ir/1.csv")
+
+    coords = df.values
+    dbscan = DBSCAN(eps=50, min_samples=100)
+    labels = dbscan.fit_predict(coords)
+    print(labels)
 
     # return id
     mlflow.log_param("run id", mlflow.active_run().info.run_id)
