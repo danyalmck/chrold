960135596723eda9bd51222c6c575b621f241b41
Daniyal-Mackey <daniyal.mackeyabadi@divar.ir>
2023-08-31 15:01:39 +0330

    Fix script

--- ct.sh
+++ ct.sh
@@ -5,5 +5,5 @@ curl -L \
 -H "Accept: application/vnd.github+json" \
 -H "Authorization: Bearer <ACCESS TOKEN>" \
 -H "X-GitHub-Api-Version: 2022-11-28" \
-https://api.github.com/repos/danyalmck/ct-test/actions/workflows/train-latest.yml/dispatches \
+https://api.github.com/repos/danyalmck/chrold/actions/workflows/train-latest.yml/dispatches \
 -d '{"ref":"main","inputs":{}}'
